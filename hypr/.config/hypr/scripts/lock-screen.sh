8305b727bb4ea2e6760f7483a0bfb9fc37702e2c
vikdov <mr.dovgoshiya@gmail.com>
2025-11-09 16:48:28 +0100

    upgrade hypridle with scripts

--- hypr/.config/hypr/scripts/lock-screen.sh
+++ hypr/.config/hypr/scripts/lock-screen.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-WP_SCRIPT="$HOME/.config/hypr/scripts/random-lock-wallpaper.sh"
-LOCK_IMAGE="/tmp/lock_wallpaper.jpg"
-
-"$WP_SCRIPT"
-
-# Wait max 1 second for a proper image
-for i in {1..20}; do
-  [[ -f "$LOCK_IMAGE" && $(stat -c%s "$LOCK_IMAGE") -gt 20000 ]] && break
-  sleep 0.05
-done
-
-exec hyprlock
+"$HOME/.config/hypr/scripts/random-lock-wallpaper.sh"
+exec hyprlock --immediate
